37d8b353f92922a7d9a5f7e6aa9a27ffdfae0f49
Szymon Halastra <szymon.halastra@gmail.com>
2020-09-26 20:42:01 +0200

    Initial commit

---
+++ src/test/java/io/shalastra/machinemetrics/MachineMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shalastra.machinemetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
