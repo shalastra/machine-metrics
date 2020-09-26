3b1072f43a2c8b33e4300b98abb021cc255f91f2
Szymon Halastra <szymon.halastra@gmail.com>
2020-09-26 20:53:57 +0200

    Set up Oshi and collect random data about cpu

--- src/main/java/io/shalastra/machinemetrics/MachineMetricsApplication.java
+++ src/main/java/io/shalastra/machinemetrics/MachineMetricsApplication.java
@@ -1,13 +1,41 @@
 package io.shalastra.machinemetrics;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
 
+@Slf4j
 @SpringBootApplication
-public class MachineMetricsApplication {
+public class MachineMetricsApplication implements CommandLineRunner {
+
+	@Bean
+	public SystemInfoService systemInfoService() {
+		return new SystemInfoService();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MachineMetricsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(systemInfoService().collect());
+	}
+}
+
+class SystemInfoService {
+
+	public String collect() {
+		SystemInfo systemInfo = new SystemInfo();
+
+		HardwareAbstractionLayer hal = systemInfo.getHardware();
+		CentralProcessor cpu = hal.getProcessor();
+
+		return cpu.toString();
+	}
 }
